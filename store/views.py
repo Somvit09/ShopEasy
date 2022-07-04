d31f7c0b05604e411b40eddd67b0bff1433fd433
Somvit09 <developer.09.sam@gmail.com>
2022-07-04 23:51:24 +0530

    added functionality of searching by slug

--- store/views.py
+++ store/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+from category.models import Category
 
 
 # Create your views here.
-def storeHome(request):
-    products = Product.objects.all().filter(is_available=True)
+def storeHome(request, category_slug=None):
+    if category_slug is not None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
     data = {
         'products': products,
+        'product_count': product_count,
     }
     return render(request, 'store.html', data)
