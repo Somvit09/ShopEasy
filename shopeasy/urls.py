8268e66a109d4c1d11f176805b7d091dcabe23ba
Somvit09 <developer.09.sam@gmail.com>
2022-08-06 23:55:05 +0530

    small changes

--- shopeasy/urls.py
+++ shopeasy/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('accounts/', include('accounts.urls')),
     path('orders/', include('Order.urls')),
-    path('admin/', admin.site.urls),
+    path('SecuREsiteOfaDMiNAccesseDonLYForMyself/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
