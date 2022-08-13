d6c700aeeaf3e1478bb7a2410e4cea9783aaf865
Somvit09 <developer.09.sam@gmail.com>
2022-08-14 00:43:17 +0530

    committed

--- shopeasy/views.py
+++ shopeasy/views.py
@@ -6,6 +6,7 @@ from store.models import Product, ReviewRating
 def home(request):
     products = Product.objects.all().filter(is_available=True).order_by('-created_date')
     categories = Category.objects.all()
+    reviews = None
     # getting all the reviews
     for product in products:
         reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
