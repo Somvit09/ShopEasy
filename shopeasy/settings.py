cf085574b2546e86b5f787b732b7ccf483bfdcc4
Somvit09 <developer.09.sam@gmail.com>
2022-08-18 12:10:32 +0530

    edited requirements.txt

--- shopeasy/settings.py
+++ shopeasy/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'shopeasy-main.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'shopeasy-main.herokuapp.com', 'localhost', '0.0.0.0']
 
 # Application definition
 
